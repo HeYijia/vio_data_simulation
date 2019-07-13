26c83604d18f67983317b198041138d3db899e0a
Zerolover <zerolover@msn.cn>
2019-07-13 16:14:31 +0800

    add build.sh, add header to imu.cpp, simplify draw_trajectory.py

--- python_tool/draw_trajcory.py
+++ python_tool/draw_trajcory.py
@@ -1,80 +1,55 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 15 18:18:24 2017
 
 @author: hyj
 """
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 22 21:43:55 2017
-
-@author: hyj
-"""
-
+import os
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-import os
-filepath=os.path.abspath('..')+"/bin"  #表示当前所处的文件夹上一级文件夹的绝对路径
+np.set_printoptions(suppress = True)
+filepath = os.path.abspath('..')+"/bin"
 
+# imu_circle   imu_spline
 position = []
 quaterntions = []
 timestamp = []
 tx_index = 5
+position = np.loadtxt(filepath + '/imu_pose.txt', usecols = (tx_index, tx_index + 1, tx_index + 2))
 
-with open(filepath + '/imu_pose.txt', 'r') as f:  # imu_circle   imu_spline
-
-    data = f.readlines()  # txt中所有字符串读入data
-    for line in data:
-        odom = line.split()  # 将单个数据分隔开存好
-        numbers_float = map(float, odom)  # 转化为浮点数
-
-        position.append([numbers_float[tx_index], numbers_float[tx_index + 1], numbers_float[tx_index + 2]])
-
+# imu_pose   imu_spline
 position1 = []
 quaterntions1 = []
 timestamp1 = []
-with open(filepath + '/imu_int_pose.txt', 'r') as f:  # imu_pose   imu_spline
+data = np.loadtxt(filepath + '/imu_int_pose.txt')
+# timestamp1 = data[:,0]
+# quaterntions1 = data[:,[tx_index + 6, tx_index + 3, tx_index + 4, tx_index + 5]] # qw,qx,qy,qz
+position1 = data[:,[tx_index, tx_index + 1, tx_index + 2]]
 
-    data = f.readlines()  # txt中所有字符串读入data
-    for line in data:
-        odom = line.split()  # 将单个数据分隔开存好
-        numbers_float = map(float, odom)  # 转化为浮点数
-
-        # timestamp.append( numbers_float[0])
-        # quaterntions1.append( [numbers_float[tx_index+6], numbers_float[tx_index+3],numbers_float[tx_index+4],numbers_float[tx_index+5]   ] )   # qw,qx,qy,qz
-        position1.append([numbers_float[tx_index], numbers_float[tx_index + 1], numbers_float[tx_index + 2]])
-
+# cam_pose_opt_o_0   cam_pose_opt_o_0
 position2 = []
 quaterntions2 = []
 timestamp2 = []
-with open(filepath + '/imu_int_pose_noise.txt', 'r') as f:  # cam_pose_opt_o_0   cam_pose_opt_o_0
+data = np.loadtxt(filepath + '/imu_int_pose_noise.txt')
+# timestamp2 = data[:,0]
+# quaterntions2 = data[:,[tx_index + 6, tx_index + 3, tx_index + 4, tx_index + 5]] # qw,qx,qy,qz
+position2 = data[:,[tx_index, tx_index + 1, tx_index + 2]]
 
-    data = f.readlines()  # txt中所有字符串读入data
-    for line in data:
-        odom = line.split()  # 将单个数据分隔开存好
-        numbers_float = map(float, odom)  # 转化为浮点数
 
-        # timestamp.append( numbers_float[0])
-        # quaterntions2.append( [numbers_float[tx_index+6], numbers_float[tx_index+3],numbers_float[tx_index+4],numbers_float[tx_index+5]   ] )   # qw,qx,qy,qz
-        position2.append([numbers_float[tx_index], numbers_float[tx_index + 1], numbers_float[tx_index + 2]])
-
-    ### plot 3d
+### plot 3d
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-xyz = zip(*position)
-xyz1 = zip(*position1)
-xyz2 = zip(*position2)
-print
-ax.plot(xyz[0], xyz[1], xyz[2], label='gt')
-ax.plot(xyz1[0], xyz1[1], xyz1[2], label='imu_int')
-# ax.plot(xyz2[0], xyz2[1], xyz2[2], label='noise')
-ax.legend()
+ax.plot(position[:,0], position[:,1], position[:,2], label='gt')
+ax.plot(position1[:,0], position1[:,1], position1[:,2], label='imu_int')
+# ax.plot(position2[:,0], position2[:,1], position2[:,2], label='noise')
+ax.plot([position[0,0]], [position[0,1]], [position[0,2]], 'r.', label='start')
 
+ax.legend()
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
--- src/imu.cpp
+++ src/imu.cpp
@@ -2,6 +2,7 @@
 // Created by hyj on 18-1-19.
 //
 
+#include <random>
 #include "imu.h"
 #include "utilities.h"
 

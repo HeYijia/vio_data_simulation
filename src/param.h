6c2a407e76653b89427615f5ec7de97d3571323d
HeYijia <heyijia_2013@163.com>
2019-08-13 22:26:25 +0800

    Update param.h

--- src/param.h
+++ src/param.h
@@ -28,8 +28,8 @@ public:
     //double gyro_bias_sigma = 1.0e-5;
     //double acc_bias_sigma = 0.0001;
 
-    double gyro_noise_sigma = 0.015;    // rad/s
-    double acc_noise_sigma = 0.019;      //　m/(s^2)
+    double gyro_noise_sigma = 0.015;    // rad/s * 1/sqrt(hz)
+    double acc_noise_sigma = 0.019;      //　m/(s^2) * 1/sqrt(hz)
 
     double pixel_noise = 1;              // 1 pixel noise
 
